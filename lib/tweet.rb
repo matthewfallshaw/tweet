ac0560c9bb8b290c04eff5172b18bb7464ae57e3
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2009-04-22 23:46:54 +1000

    use optparse and accept source

--- lib/tweet.rb
+++ lib/tweet.rb
@@ -14,14 +14,14 @@ module Tweet
 
     def create_status(status, source = nil)
       len = status.length
-      abort "Message limit is 140 characters. You currently have #{len}." if len > 140
+      abort "Abort: Message limit is 140 characters. You currently have #{len}." if len > 140
 
       if @debug
         puts "Would have tweeted: ", status
       else
         resource = RestClient::Resource.new 'http://twitter.com/statuses/update.xml', credentials[:username], credentials[:password]
         resource.post(:status => status, :content_type => 'application/xml', :accept => 'application/xml',
-                      :source => source || credentials[:source])
+                      :source => (source || credentials[:source] || 'tweetgem'))
       end
       puts "#{len} chars" + (len == 140 ? "!\nYou rock!" : '.')
     end
@@ -30,9 +30,7 @@ module Tweet
       @credentials ||= begin
                          abort "You must create a #{CONFIG_FILE} file to use this CLI." unless File.exist?(CONFIG_FILE)
                          warn "  debug mode (not really posting)" if @debug
-                         config = YAML.load(File.read(CONFIG_FILE)).symbolize_keys
-                         config[:source] ||= 'tweetgem'
-                         config
+                         YAML.load(File.read(CONFIG_FILE)).symbolize_keys
                        end
     end
   end
